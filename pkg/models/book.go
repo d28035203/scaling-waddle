4ef7050946ed81f52d116fc7562bbbc63451a3be
d28035203 <289484834+d28035203@users.noreply.github.com>
2026-02-01 15:06:33 +0530

    feat: define Book model with create and list helpers

---
+++ pkg/models/book.go
@@ -0,0 +1,32 @@
+package models
+
+import (
+	"github.com/d28035203/scaling-waddle/pkg/config"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
+type Book struct {
+	gorm.Model
+	Name        string `json:"name"`
+	Author      string `json:"author"`
+	Publication string `json:"publication"`
+}
+
+func init() {
+	config.Connect()
+	db = config.GetDB()
+	db.AutoMigrate(&Book{})
+}
+
+func (b *Book) CreateBook() *Book {
+	db.Create(&b)
+	return b
+}
+
+func GetAllBooks() []Book {
+	var books []Book
+	db.Find(&books)
+	return books
+}
