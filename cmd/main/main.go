711063bb819ad33756f7e8a663ece56b8da726b0
d28035203 <289484834+d28035203@users.noreply.github.com>
2026-03-09 20:03:15 +0530

    fix: default host/port and fail clearly when DB_DSN is missing

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -11,9 +11,23 @@ import (
 )
 
 func main() {
-	_ = godotenv.Load()
+	if err := godotenv.Load(); err != nil {
+		log.Println("warning: .env file not found, relying on process environment")
+	}
+
+	host := os.Getenv("SERVER_HOST")
+	port := os.Getenv("SERVER_PORT")
+	if host == "" {
+		host = "0.0.0.0"
+	}
+	if port == "" {
+		port = "9010"
+	}
+
 	r := mux.NewRouter()
 	routes.RegisterBookStoreRoutes(r)
-	addr := os.Getenv("SERVER_HOST") + ":" + os.Getenv("SERVER_PORT")
+
+	addr := host + ":" + port
+	log.Printf("bookstore API listening on %s", addr)
 	log.Fatal(http.ListenAndServe(addr, r))
 }
--- pkg/config/app.go
+++ pkg/config/app.go
@@ -1,23 +1,36 @@
 package config
 
 import (
+	"fmt"
+	"log"
 	"os"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 var db *gorm.DB
 
+// Connect opens a MySQL connection using DB_DSN from the environment.
 func Connect() {
 	dsn := os.Getenv("DB_DSN")
-	d, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	if err != nil {
-		panic(err)
+	if dsn == "" {
+		log.Fatal("DB_DSN is not set")
 	}
-	db = d
+
+	conn, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Warn),
+	})
+	if err != nil {
+		log.Fatalf("failed to connect to database: %v", err)
+	}
+
+	db = conn
+	fmt.Println("connected to MySQL")
 }
 
+// GetDB returns the shared GORM connection.
 func GetDB() *gorm.DB {
 	return db
 }
